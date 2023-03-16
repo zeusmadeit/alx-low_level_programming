6f3b88e86d353559737059fe20b6c91a5b3dd86c
Abraham <codexapp44@gmail.com>
2023-03-16 09:49:44 -0700

    task4 completed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
+\* The main function calls 
+* the function puts that prints out the string passed to it*\
+
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-	return(0);
+	puts ("Programming is like building a multilingual puzzle");
+	return (0);
 }
 
