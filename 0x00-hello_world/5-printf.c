ae88cf1a3603262171c1514b2bde0367c4901608
Abraham <codexapp44@gmail.com>
2023-03-16 10:06:44 -0700

    task5 completed

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - pricipal function
+ * Return: Always returns 0 (Successful)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
