50417726a94847d7fc6c91971b1792fd2c07f6de
Abraham <codexapp44@gmail.com>
2023-03-20 06:02:41 -0700

    task 10 done

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Return 0 Success
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+
+	i = '0';
+	while (i < '9')
+	{
+		j = '1'
+		while (j <= '9')
+		{
+			if (i < j)
+			{
+				putchar(i);
+				putchar(j);
+				if (i != '8' || (i == '8' &&  j != '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
