c32ddcc10670502a4d129cf40206e03e73603848
Abraham <codexapp44@gmail.com>
2023-03-20 05:02:29 -0700

    task2 done

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: returns 0 Success
+*/
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
