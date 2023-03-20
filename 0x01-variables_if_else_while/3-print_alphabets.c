16f1814096017d72ca5698eddcc3b636b157fd4b
Abraham <codexapp44@gmail.com>
2023-03-20 05:07:59 -0700

    task3 done

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: returns 0 Success
+*/
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	for (k = 'A'; k <= 'Z'; k++)
+		putchar(k);
+	putchar('\n');
+	return (0);
+}
