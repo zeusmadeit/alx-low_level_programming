c3cd4d17265245c1a632d25de795f03f0f7ae3e9
Abraham <codexapp44@gmail.com>
2023-03-20 05:40:58 -0700

    task 8 done

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: returns 0 Success
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <=63; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
