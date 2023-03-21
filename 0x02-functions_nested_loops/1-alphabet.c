b9a0cb50bef8b1b673efb8e9f41d4f31ee6bf310
Abraham <codexapp44@gmail.com>
2023-03-21 04:32:26 -0700

    task1 done

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Return 0 always succesful
+*/
+void	print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+	_putchar("\n");
+	return(0);
+}
