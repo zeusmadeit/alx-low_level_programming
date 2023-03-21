0dce2537293a66cfd0a6482f1c0231b44b132c36
Abraham <codexapp44@gmail.com>
2023-03-21 04:50:12 -0700

    task1 done

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -12,6 +12,6 @@ void	print_alphabet(void)
 
 	for (i = 'a'; i <= 'z'; i++)
 		_putchar(i);
-	_putchar("\n");
+	_putchar('\n');
 	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
 char	_putchar(char c);
+void    print_alphabet(void);
 #endif
