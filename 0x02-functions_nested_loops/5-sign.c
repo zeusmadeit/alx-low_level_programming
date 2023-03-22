524503b7dd6c617b15953c9a6598aea454366fe7
Abraham <codexapp44@gmail.com>
2023-03-22 02:37:59 -0700

    task5 done

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,20 +2,26 @@
 #include "main.h"
 
 /**
- * _isalpha - checks for ]alphabetical character.
+ * _isalpha - checks for sign.
  * @n: integer to be checked
  *
- * Return: Return 1 succes, return 0 failure
+ * Return: Return -1 if neg. or 1 if positive else 0 if n == 0
 */
 int	print_sign(int n)
 {
 	if (n < 0)
+	{
 		_putchar('-');
 		return (-1);
+	}
 	else if (n == 0)
+	{
 		_putchar('0');
                 return (0);
+	}
 	else
+	{
 		_putchar('+');
                 return (1);
+	}
 }
