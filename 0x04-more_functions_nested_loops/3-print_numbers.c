16e7f46ec3c59f3e70ba6a1305d54a733839388e
Abraham <codexapp44@gmail.com>
2023-03-27 14:27:02 -0700

    task3 modified

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints number 0 to 9
+ */
+
+void print_numbers(void)
+{
+	char i;
+
+	for (i = '0' ; i <= '9' ; i++)
+		_putchar(i);
+	_putchar('\n');
+}
