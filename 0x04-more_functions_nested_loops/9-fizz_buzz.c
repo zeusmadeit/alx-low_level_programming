7396b20c67ed703e8a4e867ab65a478a2035d566
Abraham <codexapp44@gmail.com>
2023-03-27 15:04:31 -0700

    task9 modified

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,20 +11,20 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i <= 100; i++)
+	for (i = 1 ; i <= 100 ; i++)
 	{
-		if ((i % 3 = 0) && (i % 5 = 0))
-			printf("FizzBuzz");
-		else if (i % 3 = 0 && !(i % 5 == 0))
+		if (i % 3 == 0 && !(i % 5 == 0))
 			printf("Fizz");
-		else if (i % 5 = 0 && !(i % 3 == 0))
+		else if (i % 5 == 0 && !(i % 3 == 0))
 			printf("Buzz");
+		else if (i % 5 == 0 && i % 3 == 0)
+			printf("FizzBuzz");
 		else
-			printf(i);
-
+			printf("%d", i);
 		if (i != 100)
-			printf(' ');
+			printf(" ");
+		else
+			printf("\n");
 	}
-	printf('\n');
 	return (0);
 }
