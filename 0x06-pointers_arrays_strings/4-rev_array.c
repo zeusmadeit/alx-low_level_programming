ab07025043cbb6f04f650876658aad622f00ac1e
Abraham <codexapp44@gmail.com>
2023-03-29 16:34:03 -0700

    minor adjustments to tidy code up

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * reverse_array - reverse array
@@ -18,15 +19,4 @@ void	reverse_array(int *a, int n)
 		a[n - i] = temp;	
 		i++;
 	}
-}
-
-void	main(void)
-{
-	int arr[] = {19,4,7,1,11,12,54,38};
-	reverse_array(arr, 8);
-	for (int i = 0; i < 8; i++)
-	{
-		_putchar(arr[i]);
-	}
-	_putchar('\n');
 }
