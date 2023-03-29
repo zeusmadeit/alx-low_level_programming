a59c023f62e59a0d200b43ef22b0bcca68d1c2e5
Abraham <codexapp44@gmail.com>
2023-03-29 16:23:53 -0700

    minor adjustments to tidy code up

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,22 +1,32 @@
 #include "main.h"
 
 /**
- * _strcmp - compare
- * @s1 : pointerto char params
- * @s2 : pointer to char params
- * Return: *dest
+ * reverse_array - reverse array
+ * @a : array of int
+ * @n : array length
  */
-int _strcmp(char *s1, char *s2)
+void	reverse_array(int *a, int n)
 {
 	int i;
-	int res;
+	int temp;
 
 	i = 0;
-
-	while (s1[i] == s2[i] && (s1[i] != '\0' || s2[i] != '\0'))
+	while (i <= n / 2)
 	{
+		temp = a[i];
+		a[i] = a[n - i];
+		a[n - i] = temp;	
 		i++;
 	}
-	res = s1[i] - s2[i];
-	return (res);
+}
+
+void	main(void)
+{
+	int arr[] = {19,4,7,1,11,12,54,38};
+	reverse_array(arr, 8);
+	for (int i = 0; i < 8; i++)
+	{
+		_putchar(arr[i]);
+	}
+	_putchar('\n');
 }
