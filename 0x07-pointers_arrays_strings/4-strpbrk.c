02b8599a8a1f84faeb8e246217f6764d46cb594d
Abraham <codexapp44@gmail.com>
2023-04-04 10:53:00 -0700

    task4 done

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -10,18 +10,16 @@
  * Return: Character
  */
 
-char	*_strpbrk(char *s, char *accept)
+char *_strpbrk(char *s, char *accept)
 {
-	int i, j;
+	int i, k;
 
-	for (i = 0; s[i] !='\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; accept[j] != '\0'; j++ )
+		for (k = 0; accept[k] != '\0'; k++)
 		{
-			if (accept[j] == s[i])
-			{
+			if (s[i] == accept[k])
 				return (s + i);
-			}
 		}
 	}
 	return (0);
