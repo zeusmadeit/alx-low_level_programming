8fb929daaedb699fe9064713d1d9d5945b3a268a
Abraham <codexapp44@gmail.com>
2023-04-04 09:41:41 -0700

    task0 done

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - sets or fills the block of memory with given argument
+ * @*s: pointer to memory area
+ * @b: character to fill memory with
+ * @n: integer
+ * Return: Returns a pointer to the memory area
+*/
+
+char	*_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <unistd.h>
+
+char	*_memset(char *s, char b, unsigned int n);
+#endif
