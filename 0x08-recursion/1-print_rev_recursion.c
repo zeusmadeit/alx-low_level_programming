e7f957abfa420853a986e9795ce7a851793357c8
Abraham <codexapp44@gmail.com>
2023-04-05 03:40:48 -0700

    task1 done

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -7,11 +7,9 @@
 
 void	_print_rev_recursion(char *s)
 {
-	if (*s)
+	if (*s != '\0')
 	{
-		_print-rev_recursion(*s);
-		_putchar(*s++);
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
-	else
-		_putchar('\n');
 }
