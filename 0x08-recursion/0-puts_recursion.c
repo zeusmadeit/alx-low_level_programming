c5ce08e8099092cedec3dd3f0be27c90566fcc06
Abraham <codexapp44@gmail.com>
2023-04-05 03:10:10 -0700

    task0 done

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string, followed by a new line
+ * @s: string
+ */
+
+void	_puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s++);
+		_puts_recursion(s);
+	}
+	else
+		_putchar('\n');
+}
