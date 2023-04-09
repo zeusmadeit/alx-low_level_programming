338197f1916a8a16d131d21d3b87ed2dd5fd50bc
Abraham <codexapp44@gmail.com>
2023-04-09 02:29:31 -0700

    advanced task completed

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,6 +1,19 @@
 #include "main.h"
 
 /**
+ * _strlen_recursion - size
+ * @s: string
+ * Return: recursion
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+		return (1 + _strlen_recursion(s + 1));
+	return (0);
+}
+
+/**
  * pal_checker - check if is palindrome
  * @s: string
  * @l: left index
