bec8564591775eb6639c6e71462db0f20001119b
Abraham <codexapp44@gmail.com>
2023-04-09 04:13:45 -0700

    task4 done

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+/**
+ * isNum - check if char in string array is num
+ * @num: string
+ * Return: 0 if it's num, 1 if not
+ */
+
+int isNum(char num[])
+{
+	int i, l = strlen(num);
+
+	for (i = 0 ; i < l ; i++)
+		if (!isdigit(num[i]))
+			return (1);
+	return (0);
+}
+
+/**
+ * main - adds positive numbers
+ * @argc: num of arg
+ * @argv: cmd line arg
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, sum;
+
+	if (argc == 1)
+		printf("0\n");
+	else
+	{
+		sum = 0;
+		for (i = 1 ; i < argc ; i++)
+		{
+			if (isNum(argv[i]) == 0)
+				sum += atoi(argv[i]);
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		printf("%d\n", sum);
+	}
+	return (0);
+}
