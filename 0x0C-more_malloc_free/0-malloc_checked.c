af591cd9494c12aa72580edf02faacf4f01674a1
Abraham <codexapp44@gmail.com>
2023-04-13 16:04:09 -0700

    task0 done

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+* malloc_checked - allocates memory using malloc
+* @b: integer
+*/
+void	*malloc_checked(unsigned int b)
+{
+	void *mem;
+
+	mem = malloc(b);
+	if (mem == NULL)
+		exit(98);
+	return (mem);
+}
