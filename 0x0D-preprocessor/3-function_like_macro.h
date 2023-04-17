be01e72f4c9c0ed6826d2e7734d8c33c78709fd5
Abraham <codexapp44@gmail.com>
2023-04-17 14:53:23 -0700

    task3 done

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,5 +1,9 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
+/**
+ * ABS - Macro to return absolute value of @x
+ * @x: argument passed to ABS
+ */
 #define ABS(x) (x < 0 ? x * (- 1) : x)
 
 #endif
