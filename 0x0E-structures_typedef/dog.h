5a724dd28baadd7b179ecef8971429517f79a31d
Abraham <codexapp44@gmail.com>
2023-04-17 14:50:19 -0700

    task0 done

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
-struct dog{
+/**
+*struct dog - structure representing a dog
+*@name: the dog's name
+*@age: the dog's age
+*@owner: owner's name
+*/
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
