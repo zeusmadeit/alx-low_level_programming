0b1d0ec05418e7e8fc35156e62ac779687f4fe50
Abraham <codexapp44@gmail.com>
2023-04-20 08:43:02 -0700

    task1 done

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,13 +9,11 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int i = 0;
+	int *last = array + size - 1;
 
 	if (array && size && action)
 	{
-		while (i++ < size)
-		{
-			action(array[i]);
-		}
+		while (array <= last)
+			action(*array++);
 	}
 }
