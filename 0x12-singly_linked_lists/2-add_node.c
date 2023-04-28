8c9117bfa2db322055de3ca0749aec753d07a823
Abraham <codexapp44@gmail.com>
2023-04-28 10:10:30 -0700

    task2 done

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+ * _strlen - returns length of str
+ * @s: char
+ * Return: c
+ */
+int _strlen(const char *s)
+{
+	int c = 0;
+
+	while (s[c] != '\0')
+		c++;
+	return (c);
+}
+
+/**
+ * add_node - adds a new node @ start of a list_t list
+ * @head: head of list
+ * @str: string
+ * Return: address of the new element, or NULL if it failed
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *ad;
+	unsigned int l = 0;
+
+	while (str[l])
+		l++;
+	ad = malloc(sizeof(list_t));
+	if (ad == NULL)
+		return (NULL);
+	ad->str = strdup(str);
+	ad->len = l;
+	ad->next = *head;
+	*head = ad;
+	return (*head);
+}
