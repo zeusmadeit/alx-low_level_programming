2fbfab07a92854704d1823bfc82b348ef01a76c7
Abraham <codexapp44@gmail.com>
2023-04-29 09:28:41 -0700

    task2 done

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,14 +8,14 @@
 
 size_t	print_listint(const listint_t *h)
 {
-	size_t i = 0;
+	size_t i;
 
+	i = 0;
 	while (h)
 	{
-		printf("%d\n", h->n);
 		i++;
+		printf("%d\n", h->n);
 		h = h->next;
 	}
 	return (i);
 }
-
