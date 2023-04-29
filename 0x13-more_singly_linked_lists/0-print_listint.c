7addc58626f35fc281717ff160ad77ae1409fcd4
Abraham <codexapp44@gmail.com>
2023-04-29 09:05:53 -0700

    task0 complete

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a listint_t list.
+ * @h: listint_t list
+ * Return: size of listint_t list @h
+ */
+
+size_t	print_listint(const listint_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		printf("%d", h->n);
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
+
