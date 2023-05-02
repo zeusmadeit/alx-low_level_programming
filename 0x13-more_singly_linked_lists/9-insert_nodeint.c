bdde6fcbf243f4a38553f2c8508549048b00de57
Abraham <codexapp44@gmail.com>
2023-05-02 04:13:07 -0700

    task9 done

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,7 +23,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		new_node->next = temp;
 		temp = new_node;
-		return (temp);
+		return (new_node);
 	}
 	while (temp)
 	{
@@ -31,7 +31,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		{
 			new_node->next = temp->next;
 			temp->next = new_node;
-			return (temp);
+			return (new_node);
 		}
 		temp = temp->next;
 		i++;
