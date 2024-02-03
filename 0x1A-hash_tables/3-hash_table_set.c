7a3cd6e21dd4572cee6d611ee50da3884c5d237c
ABRAHAM PAUL <blackapp44@gmail.com>
2024-02-03 03:00:26 +0000

    added function hash_table_set to add an element to the hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,73 +1,53 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_set - adds an element to the hash table.
+ * hash_table_set - Add or update an element in a hash table.
+ * @ht: A pointer to the hash table.
+ * @key: The key to add - cannot be an empty string.
+ * @value: The value associated with key.
  *
- * @key: key to calculate index from.
- * @value: value to be saved
- *
- * Return: returns 1 upon success or 0 if failed
+ * Return: 0 if failed, Otherwise return 1.
  */
+
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    hash_node_t *head;
-    hash_node_t *temp;
-    char *s;
-    unsigned long int index;
+	hash_node_t *new;
+	char *value_copy;
+	unsigned long int index, i;
 
-    strcpy(s, value);
-    index = key_index(key, ht->size);
-    temp = malloc(sizeof(hash_node_t));
-    if(temp == NULL)
-    {
-        return(0);
-    }
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
 
-    if(ht->array[index] == NULL)
-    {
-        temp->key = key;
-        temp->value = value;
-        temp->next = NULL;
-        ht->array[index] = temp;
-    }
-    else
-    {
-        head = ht->array[index];
-        if(!(has_key(head, key))) // check if key exist at index
-        {
-            while (ht->array[index]->next != NULL)
-            {
-                ht->array[index] = ht->array[index]->next;
-            }
-            temp->key = key;
-            temp->value = value;
-            temp->next = NULL;
-            ht->array[index]->next = temp;
-        }
-    }
+	value_copy = strdup(value);
+	if (value_copy == NULL)
+		return (0);
 
-    free(temp);
-    return(1);
-}
+	index = key_index((const unsigned char *)key, ht->size);
+	for (i = index; ht->array[i]; i++)
+	{
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = value_copy;
+			return (1);
+		}
+	}
 
-/**
- * has_key - checks if the given index already has a node with the given key
- *
- * @head: the linked list to traverse
- * @index: the given index
- * @key: key to calculate index from.
- *
- * Return: returns 1 if it exist or 0 if it doesnt
- */
-int has_key(hash_node_t *head, const char *key)
-{
-    while (head != NULL)
-    {
-        if(strcmp(head->key, key) == 0)
-        {
-            return(1);
-        }
-    }
-    return(0);
-    
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+	{
+		free(value_copy);
+		return (0);
+	}
+	new->key = strdup(key);
+	if (new->key == NULL)
+	{
+		free(new);
+		return (0);
+	}
+	new->value = value_copy;
+	new->next = ht->array[index];
+	ht->array[index] = new;
+
+	return (1);
 }
