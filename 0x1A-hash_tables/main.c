7495b039bb1af7f74bd57db248737d9f810494c6
ABRAHAM PAUL <blackapp44@gmail.com>
2024-02-07 13:26:49 +0000

    redoing project Hash Tables, Exercise 5-hash_table_print

--- 0x1A-hash_tables/main.c
+++ 0x1A-hash_tables/main.c
@@ -7,23 +7,18 @@
  */
 int main()
 {
-    char *s;
     hash_table_t *ht;
 
-    s = "cisfun";
     ht = hash_table_create(1024);
     
-    printf("HashTable address: %p\n", (void *)ht);
-    printf("Size of array: %lu\n", ht->size);
-    printf("\n");
-    printf("\n");
-
-    printf("%s => hash: %lu, key: %lu\n", s, hash_djb2((unsigned char *)s), key_index((unsigned char *)s, ht->size));
-    
-    s = "Don't forget to tweet today";
-    printf("%s => hash: %lu, key: %lu\n", s, hash_djb2((unsigned char *)s), key_index((unsigned char *)s, ht->size));
-    
-    s = "98";
-    printf("%s => hash: %lu, key: %lu\n", s, hash_djb2((unsigned char *)s), key_index((unsigned char *)s, ht->size));
+    hash_table_print(ht);
+    hash_table_set(ht, "c", "fun");
+    hash_table_set(ht, "python", "awesome");
+    hash_table_set(ht, "Bob", "and Kris love asm");
+    hash_table_set(ht, "N", "queens");
+    hash_table_set(ht, "Asterix", "Obelix");
+    hash_table_set(ht, "Betty", "Cool");
+    hash_table_set(ht, "98", "Battery Street");
+    hash_table_print(ht);
     return (EXIT_SUCCESS);
 }
